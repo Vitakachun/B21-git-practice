27b37b1ddc4415d6af2e3f363ecd1689164c0bb1
Vitakachun <kachunvita@gmail.com>
2021-01-26 14:23:22 -0600

    movie 1 more work here

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
-        System.out.println("B21 future sdets learning Git");
+        System.out.println("B21 future SDET learning Git");
+        // watching more movie in bedroom
     }
 }
